cf2d82e13e9e3afff1175b420459202c7516f7c5
alexrayo <alexrayo.net@gmail.com>
2020-07-10 13:11:17 -0600

    Updates

---
+++ precache-manifest.da0d53f49e00969622249c4d1ae9a29a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ed151a1bedd952b5fe8fc7180e3699b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c697cdc29dacf0febdb0",
+    "url": "/static/css/main.3496c208.chunk.css"
+  },
+  {
+    "revision": "c0bd6bbcfb898fe83709",
+    "url": "/static/js/2.5cd6db24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5cd6db24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c697cdc29dacf0febdb0",
+    "url": "/static/js/main.8595f66b.chunk.js"
+  },
+  {
+    "revision": "50a3ceb2cff5463c61b5",
+    "url": "/static/js/runtime-main.ffe76a0c.js"
+  }
+]);
